48078c79706c592ee406941017fbf42b76b586f6
AJ Priola <ajpriola@gmail.com>
2017-09-25 23:05:09 -0400

    Initial structure for MovieView

--- src/views/MovieView/index.js
+++ src/views/MovieView/index.js
@@ -4,20 +4,46 @@ import PropTypes from 'prop-types';
 import './movieview.css';
 
 class MovieView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {}
+    };
+
+    this.getMovie = this.getMovie.bind(this);
+  }
+
   componentWillMount() {
+    this.getMovie();
+  }
+
+  getMovie() {
     const id = this.props.match.params.id;
-
     const url = `https://api.themoviedb.org/3/movie/${id}?api_key=89b8b7adcb4465f94974e82a5a1f77e3&language=en-US`;
-
     fetch(url)
       .then(results => results.json())
       .then((data) => {
-        console.log(data);
+        this.setState({ movie: data });
       });
   }
 
   render() {
-    return <p>{this.props.match.params.id}</p>;
+    const movie = this.state.movie;
+    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const year = new Date(movie.release_date).getFullYear();
+
+    return (
+      <div className="movie-container">
+        <div className="movie-header">
+          <img className="movie-image" src={posterUrl} alt="" />
+          <div className="title-date-container">
+            <h3 className="movie-title">{movie.title}</h3>
+            <h3 className="movie-date">{year}</h3>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
